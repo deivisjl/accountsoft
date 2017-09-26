bd5af8355e46744226aeb8dac6cab6ec33131cbd
deivisjl <deivisjl@gmail.com>
2017-09-26 06:06:01 -0600

    Initial commit
    Obteniendo instancia inicial del proyecto

---
+++ database/migrations/2017_09_26_110658_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('rolid')->unsigned();
+            $table->foreign('rolid')->references('id')->on('rol');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
