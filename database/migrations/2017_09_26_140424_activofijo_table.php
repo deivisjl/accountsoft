cbd1a9dd1a849ab213e88c9b3fe093ffd2bed1a2
deivisjl <deivisjl@gmail.com>
2017-09-26 08:16:51 -0600

    Creacion de migracion activofijo
    Comando artisan:
    php artisan make:migration activofijo_table --create=activofijo

---
+++ database/migrations/2017_09_26_140424_activofijo_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ActivofijoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activofijo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->integer('cantidad');
+            $table->decimal('costo',9,2);
+            $table->foreign('categoriaid')->references('id')->on('categoriaactivo');
+            $table->integer('metodoid')->unsigned();
+            $table->foreign('metodoid')->references('id')->on('metododeprec');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activofijo');
+    }
+}
